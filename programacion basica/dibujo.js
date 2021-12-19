f17e6de1d79ff2b35be19c12e8ff19a9e133a6dc
jhonatan <jhonatansmith90@gmail.com>
2021-12-19 13:15:37 -0500

    first commit

---
+++ programacion basica/dibujo.js
@@ -0,0 +1,9 @@
+var d= document.getElementById("dibujito")
+var lienzo = d-getContext("2d")
+
+dibujarLinea("#AAF"), 0, 0, 10, 300);
+dibujarLinea("#AAF"), 0, 10, 20, 300);
+dibujarLinea("#AAF"), 0, 20, 20, 300);
+
+
+Function dibujarLinea(color, xinicial, yinicial)
